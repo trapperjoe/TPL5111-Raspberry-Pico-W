1f6be9d186470e60196a0ce363803e526985cad4
Johannes Marko <147028901+trapperjoe@users.noreply.github.com>
2024-10-11 10:30:27 +0200

    Update Test_TPL5111.py

--- Test_TPL5111.py
+++ Test_TPL5111.py
@@ -4,15 +4,17 @@ import utime
 DONE = Pin(0, Pin.OUT) # setting this pin High will inform the TPL5111 that the job was done and the sleep period can start now.
 adc0 = ADC(0) # Open ADC0 (Pin26)
 
-icounter = 0     # counter for measurement
+icounter = 0                # counter for measurement
 while icounter <= 10:
-    icounter  += 1  # Increase counter 
+    icounter  += 1          # Increase counter 
     read0 = adc0.read_u16() #  Read value from ADC0
+    tstring = ''            # Define a simple test string
 
     # Opening a log file with "a" for appending
     with open("log_status.txt", "a") as logFil:
         tstring = str(icounter) + '\t' + str(read0)
-        logFil.write(str(tstring))
+        print("tstring = ", tstring)
+        logFil.write(tstring)
         logFil.write('\n') # Add a new line
         logFil.flush() # Write to file 
         logFil.close() # Ensure data is written
