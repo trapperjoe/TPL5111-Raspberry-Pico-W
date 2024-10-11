e799f5cbaf4a066a054d4258810190da6fd2b8c3
Johannes Marko <147028901+trapperjoe@users.noreply.github.com>
2024-10-11 10:17:31 +0200

    Update Test_TPL5111.py

--- Test_TPL5111.py
+++ Test_TPL5111.py
@@ -3,7 +3,7 @@ from machine import Pin, ADC
 DONE = Pin(0, Pin.OUT) # setting this pin High will inform the TPL5111 that the job was done and the sleep period can start now.
 adc0 = ADC(0) # Open ADC0 (Pin26)
 
-read0 = adc.read_u16() #  Read value from ADC0
+read0 = adc0.read_u16() #  Read value from ADC0
 
 # Opening a log file with "a" for appending
 with open("log_status.txt", "a") as logFil:
